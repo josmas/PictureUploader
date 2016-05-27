72165a90f3cb3edc5b2c5126efe492ca038bbeb9
josmas <josmasflores@gmail.com>
2016-05-27 19:22:15 +0100

    new activity to take a picture set

--- app/src/main/java/org/jos/pictureuploader/PicturesActivity.java
+++ app/src/main/java/org/jos/pictureuploader/PicturesActivity.java
@@ -1,45 +1,24 @@
 package org.jos.pictureuploader;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.view.View;
 
-public class PictureListActivity extends AppCompatActivity {
-
-  ListView listView;
+public class PicturesActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_picture_list);
+    setContentView(R.layout.activity_pictures);
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
 
-    final Intent picturesActivityIntent = new Intent(this, PicturesActivity.class);
-    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-    fab.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-            .setAction("Action", null).show();
-        startActivity(picturesActivityIntent);
-      }
-    });
-
-    listView = (ListView) findViewById(R.id.list);
-    String[] values = new String[] { "No items to show - take some pictures?" };
-    ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-        android.R.layout.simple_list_item_1, android.R.id.text1, values);
-    listView.setAdapter(adapter);
-
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
   }
 
   @Override
@@ -63,4 +42,5 @@ public class PictureListActivity extends AppCompatActivity {
 
     return super.onOptionsItemSelected(item);
   }
+
 }
