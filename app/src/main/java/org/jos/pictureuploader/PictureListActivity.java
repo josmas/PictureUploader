d3ca07fcaf80d5a898dc9288c6baa0a411864011
josmas <josmasflores@gmail.com>
2016-05-29 23:59:57 +0100

    adds button to upload pending tasks from the main activity

--- app/src/main/java/org/jos/pictureuploader/PictureListActivity.java
+++ app/src/main/java/org/jos/pictureuploader/PictureListActivity.java
@@ -4,13 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.io.File;
@@ -31,13 +32,13 @@ public class PictureListActivity extends AppCompatActivity {
     fab.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-            .setAction("Action", null).show();
         startActivity(picturesActivityIntent);
       }
     });
 
     listView = (ListView) findViewById(R.id.list);
+    Button uploadNow = (Button) findViewById(R.id.upload_button);
+    uploadNow.setVisibility(View.GONE);
 
     //TODO (jos) the following code would benefit from an AsyncTask and a loading gif.
     // At this stage it is safe to delete any CHEST_EYES_TOP picture hanging around
@@ -51,16 +52,30 @@ public class PictureListActivity extends AppCompatActivity {
       }
     }
     listOfFiles = path.listFiles(); // With the other files deleted - these are files still to send.
-    int arrayLength = (listOfFiles.length > 0) ? listOfFiles.length : 1;
+    int arrayLength = 1; // At least one spot for the 'no items scheduled...' bit.
+    if (listOfFiles.length > 0) {
+      arrayLength = listOfFiles.length;
+      uploadNow.setVisibility(View.VISIBLE);
+    }
     String[] values = new String[arrayLength];
     values[0] = "No items scheduled for upload.";
     for (int i = 0; i < listOfFiles.length; i++) {
-      values[i] = listOfFiles[i].getAbsolutePath();
+      values[i] = listOfFiles[i].getName();
     }
     ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
         android.R.layout.simple_list_item_1, android.R.id.text1, values);
     listView.setAdapter(adapter);
+    uploadNow.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        uploadFilesNow();
+      }
+    });
+  }
 
+  private void uploadFilesNow() {
+    //TODO (jos) implement upload in a task
+    Log.i("PUL", "Implement upload in a thread or asyncTask - can I get a callback with AsyncTask?");
   }
 
   @Override
