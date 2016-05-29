a217d3998b413b333b2a991c72b51c47c81f813c
josmas <josmasflores@gmail.com>
2016-05-29 18:32:29 +0100

    show files still to be uploaded in main activity list

--- app/src/main/java/org/jos/pictureuploader/PictureListActivity.java
+++ app/src/main/java/org/jos/pictureuploader/PictureListActivity.java
@@ -2,6 +2,7 @@ package org.jos.pictureuploader;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +12,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.io.File;
 
 public class PictureListActivity extends AppCompatActivity {
 
@@ -35,7 +38,25 @@ public class PictureListActivity extends AppCompatActivity {
     });
 
     listView = (ListView) findViewById(R.id.list);
-    String[] values = new String[] { "No items to show - take some pictures?" };
+
+    //TODO (jos) the following code would benefit from an AsyncTask and a loading gif.
+    // At this stage it is safe to delete any CHEST_EYES_TOP picture hanging around
+    File path = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+    File[] listOfFiles = path.listFiles();
+    for (File file : listOfFiles) {
+      if (file.getName().contains("CHEST") ||
+          file.getName().contains("EYE") ||
+          file.getName().contains("TOP")) {
+        file.delete();
+      }
+    }
+    listOfFiles = path.listFiles(); // With the other files deleted - these are files still to send.
+    int arrayLength = (listOfFiles.length > 0) ? listOfFiles.length : 1;
+    String[] values = new String[arrayLength];
+    values[0] = "No items scheduled for upload.";
+    for (int i = 0; i < listOfFiles.length; i++) {
+      values[i] = listOfFiles[i].getAbsolutePath();
+    }
     ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
         android.R.layout.simple_list_item_1, android.R.id.text1, values);
     listView.setAdapter(adapter);
